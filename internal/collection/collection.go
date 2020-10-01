fddba094fa623752f6c627fd15d7360eab43dbe3
Brendan Ashworth <brendan.ashworth@me.com>
2020-10-01 00:54:31 -0600

    temporarily switch to my geoindex

--- internal/collection/collection.go
+++ internal/collection/collection.go
@@ -3,8 +3,8 @@ package collection
 import (
 	"runtime"
 
+	"github.com/brendanashworth/geoindex"
 	"github.com/tidwall/btree"
-	"github.com/tidwall/geoindex"
 	"github.com/tidwall/geojson"
 	"github.com/tidwall/geojson/geo"
 	"github.com/tidwall/geojson/geometry"
@@ -765,7 +765,7 @@ func (c *Collection) Nearby(
 		cursor.Step(offset)
 	}
 	c.index.Nearby(
-		geoindex.SimpleBoxAlgo(
+		SimpleBox(
 			[2]float64{center.X, center.Y},
 			[2]float64{center.X, center.Y},
 		),
@@ -791,4 +791,45 @@ func nextStep(step uint64, cursor Cursor, deadline *deadline.Deadline) {
 	if cursor != nil {
 		cursor.Step(1)
 	}
+}
+
+// SimpleBox performs box distance algorithm on rectangles.
+func SimpleBox(targetMin, targetMax [2]float64) func(min, max [2]float64, data interface{}, item bool) float64 {
+	return func(min, max [2]float64, data interface{}, item bool) float64 {
+		return boxDist(targetMin, targetMax, min, max)
+	}
+}
+
+func boxDist(amin, amax, bmin, bmax [2]float64) float64 {
+	var dist float64
+	var min, max float64
+	if amin[0] > bmin[0] {
+		min = amin[0]
+	} else {
+		min = bmin[0]
+	}
+	if amax[0] < bmax[0] {
+		max = amax[0]
+	} else {
+		max = bmax[0]
+	}
+	squared := min - max
+	if squared > 0 {
+		dist += squared * squared
+	}
+	if amin[1] > bmin[1] {
+		min = amin[1]
+	} else {
+		min = bmin[1]
+	}
+	if amax[1] < bmax[1] {
+		max = amax[1]
+	} else {
+		max = bmax[1]
+	}
+	squared = min - max
+	if squared > 0 {
+		dist += squared * squared
+	}
+	return dist
 }
